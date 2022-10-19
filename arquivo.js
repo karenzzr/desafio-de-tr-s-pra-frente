a98ce3aa7e32f89ab5affee59c8f7b2f7302186d
Karen Helena <helenakaren55@gmail.com>
2022-10-19 17:50:12 -0300

    enviando o desafio (de trás para frente)

---
+++ arquivo.js
@@ -0,0 +1,12 @@
+var valores = qualquercoisa
+
+prompt("digite um valor qualquer")
+
+
+for (var contador = valores.length; contador < 0; contador--) {
+    console.log(valores{ contador })
+}
+
+alert(" o valor digitado de trás para frente fica equivalente á" + valores)
+
+
